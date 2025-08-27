32532bb381374231ec54331e0f4ad738f7e352c8
Alonso <alonsohernandez3005@gmail.com@gmail.com>
2025-08-27 22:57:41 +0200

    Course created

---
+++ src/src/Manager/Course.java
@@ -0,0 +1,31 @@
+package Manager;
+
+public class Course {
+    private String name;
+    private double mark;
+
+    public Course(String name, double mark) {
+        this.name = name;
+        this.mark = mark;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMark() {
+        return mark;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMark(double mark) {
+        this.mark = mark;
+    }
+
+    public void show(){
+        System.out.println("The course name is: " + name+" and its mark is: " + mark);
+    }
+}
---
+++ src/src/Manager/Main.java
@@ -0,0 +1,12 @@
+package Manager;
+
+
+import javax.security.auth.Subject;
+
+public class Main {
+    public static void main(String[] args) {
+        Course c1 = new Course("Java Programming", 1.0);
+        c1.show();
+
+    }
+    }
